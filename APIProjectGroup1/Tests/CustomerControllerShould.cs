19d522c7b21db018293bff80e6f393ea7b2e3cf2
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-10-24 12:38:20 +0100

    Adding moq dependency

---
+++ APIProjectGroup1/Tests/CustomerControllerShould.cs
@@ -0,0 +1,28 @@
+﻿using APIProjectGroup1.Controllers;
+using APIProjectGroup1.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class CustomerControllerShould
+    {
+        private CustomersController _controller;
+
+        [Category("Controller Constructor")]
+        [Category("Happy Path")]
+        [Test]
+        public void BeAbleToConstruct()
+        {
+            var mockObject = new Mock<ICustomerService>();
+            _controller = new CustomersController(mockObject.Object);
+            Assert.That(_controller, Is.TypeOf<CustomerController>());
+        }
+
+
+    }
+}
