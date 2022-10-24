e5819286d1b4d8ad7f19dafdbd4dc0d2600cc6eb
Ameer58 <aimam58@gmail.com>
2022-10-24 12:30:47 +0100

    Added service layer

--- APIProjectGroup1/APIProjectGroup1/Services/CustomerService.cs
+++ APIProjectGroup1/APIProjectGroup1/Services/CustomerService.cs
@@ -1,6 +1,42 @@
-﻿namespace APIProjectGroup1.Services
+﻿using APIProjectGroup1.Models;
+
+namespace APIProjectGroup1.Services
 {
     public class CustomerService : ICustomerService
     {
+        private readonly NorthwindContext _context;
+        public CustomerService()
+        {
+            _context = new NorthwindContext();
+        }
+        public CustomerService(NorthwindContext context)
+        {
+            _context = context;
+        }
+    
+        public async Task CreateCustomerAsync(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Customer> GetCustomerByIdAsync(string CustomerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public  List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task RemoveCustomerAsync(Customer c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveCustomerChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
