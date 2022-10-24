93df3824837346682f28a52f37a609bf5c5fad46
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-10-24 17:01:24 +0100

    Fixed some issues

--- APIProjectGroup1/Tests/CustomerControllerShould.cs
+++ APIProjectGroup1/Tests/CustomerControllerShould.cs
@@ -24,8 +24,6 @@ namespace Tests
             var mockObject = new Mock<ICustomerService>();
             _controller = new CustomersController(mockObject.Object);
             Assert.That(_controller, Is.TypeOf<CustomersController>());
-<<<<<<< HEAD
-=======
         }
         [Category("Create")]
         [Category("Happy Path")]
@@ -57,7 +55,6 @@ namespace Tests
             var result = await _controller.GetCustomer(It.IsAny<string>());
 
             Assert.That(result.Value, Is.TypeOf<Customer>());
->>>>>>> dev
         }
     }
 }
