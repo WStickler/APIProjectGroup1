c93a1860b36b182425f4cf6043acfe9efcbd0074
Sergiusz Pietrala <pietralasergiusz@gmail.com>
2022-10-24 17:00:54 +0100

    Adding more moq tests

--- APIProjectGroup1/Tests/CustomerControllerShould.cs
+++ APIProjectGroup1/Tests/CustomerControllerShould.cs
@@ -25,7 +25,7 @@ namespace Tests
             _controller = new CustomersController(mockObject.Object);
             Assert.That(_controller, Is.TypeOf<CustomersController>());
         }
-        [Category("Create")]
+        [Category("Get Customer")]
         [Category("Happy Path")]
         [Test]
         public async Task GetCustomer_GivenCustomer_ReturnsCustomer()
@@ -41,20 +41,67 @@ namespace Tests
 
             Assert.That(result.Value, Is.EqualTo(customer));
         }
-        [Category("Create")]
+        [Category("Get Customer")]
         [Category("Sad Path")]
         [Test]
-        public async Task GetCustomer_GivenNull_ReturnsError()
+        public async Task GetCustomer_GivenNull_ReturnsNull()
         {
             var mockObject = new Mock<ICustomerService>();
             _controller = new CustomersController(mockObject.Object);
             mockObject.Setup(x =>
             x.GetCustomerByIdAsync(It.IsAny<string>()).Result)
-                .Returns((Customer) null);
+                .Returns((Customer)null);
 
             var result = await _controller.GetCustomer(It.IsAny<string>());
 
-            Assert.That(result.Value, Is.TypeOf<Customer>());
+            Assert.That(result.Value, Is.Null);
+        }
+
+        [Category("Get Customer By Search")]
+        [Category("Happy Path")]
+        [Test]
+        public async Task GetCustomerBySearch_GivenCorrectData_ReturnsAList()
+        {
+            var mockObject = new Mock<ICustomerService>();
+            var customer = new Customer() { CustomerId = "SERG", ContactName = "Sergiusz Pietrala" };
+            var customer2 = new Customer() { CustomerId = "SYED", ContactName = "Syed Ahmed" };
+            _controller = new CustomersController(mockObject.Object);
+            mockObject.Setup(x =>
+            x.GetCustomerBySearchTerm(It.IsAny<string>()).Result)
+                .Returns(new List<Customer>() { customer, customer2 });
+
+            var result = await _controller.GetCustomerBySearch(It.IsAny<string>());
+
+            Assert.That(result, Is.EqualTo(new List<Customer>() { customer, customer2 }));
+            Assert.That(result, Is.TypeOf<List<Customer>>());
+        }
+        [Category("Get Customer By Search")]
+        [Category("Sad Path")]
+        [Test]
+        public async Task GetCustomerBySearch_GivenIncorrect_ReturnsNull()
+        {
+            var mockObject = new Mock<ICustomerService>();
+            _controller = new CustomersController(mockObject.Object);
+            mockObject.Setup(x =>
+            x.GetCustomerBySearchTerm(It.IsAny<string>()).Result)
+                .Returns((List<Customer>)null);
+
+            var result = await _controller.GetCustomerBySearch(It.IsAny<string>());
+
+            Assert.That(result, Is.Null);
+        }
+        [Ignore("Not implemented")]
+        [Category("Post Customer")]
+        [Category("Happy Path")]
+        [Test]
+        public async Task GivenCustomer_WhichIsNotInTheDatabase_ReturnsCreatedAtAction()
+        {
+            var mockObject = new Mock<ICustomerService>();
+            var customer = new Customer() { CustomerId = "SERG", ContactName = "Sergiusz Pietrala" };
+            _controller = new CustomersController(mockObject.Object);
+            //mockObject.Setup(x =>
+            //x.CreateCustomerAsync(It.IsAny<Customer>()))
+            //    .Returns();
         }
     }
 }
